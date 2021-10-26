eb0a5927fb8d464d0d3c146d8e018ebacc3fdda0
Leonid Glanz <leonidglanz@users.noreply.github.com>
2021-10-26 02:00:07 +0200

    Removed handling of APKs

--- analysis/src/main/scala/classifier/Utils.scala
+++ analysis/src/main/scala/classifier/Utils.scala
@@ -14,21 +14,5 @@ object  Utils {
 
   def isLibraryType(o: Type): Boolean = {
     libraries.contains(o)
-  }
-
-  def enjarify(apkPath: String): File = {
-    val file = File.createTempFile("myJar", ".jar")
-    (getConfigValue("ENJARIFY_EXEC_CMD") + " -f " + apkPath + " -o " + file.getAbsolutePath).!!
-    file
-  }
-
-  private val props = new Properties()
-
-  def getConfigValue(value: String): String = {
-    if (props.isEmpty) {
-      props.load(new FileReader("config.txt"))
-    }
-    props.getProperty(value)
-  }
-  
+  }  
 }
